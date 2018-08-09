dd98582a8d78930f1564157937a8a2e4324b0a5e
fran <fmls1989@gmail.com>
2018-08-09 21:15:58 +0200

    add mongoose

--- configs/db.config.js
+++ configs/db.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const DB_NAME = 'facebook';
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI,  { useMongoClient: true })
+mongoose.connect(MONGO_URI)
 .then(()=>{
     console.log(`connected to database: ${MONGO_URI}`);   
 })
